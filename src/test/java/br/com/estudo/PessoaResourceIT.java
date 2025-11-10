cd9de0ae7401ffbc997c08927bde1e3fe7123965
Leonardo Vizagre <ava175@einstein.br>
2025-11-10 19:18:22 -0300

    Preparacao para rodar POC

--- src/test/java/br/com/estudo/PessoaResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.estudo;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class PessoaResourceIT extends PessoaResourceTest {
-    // Executa os mesmos testes em modo nativo
-    // Todos os testes da classe PessoaResourceTest serão executados
-}
